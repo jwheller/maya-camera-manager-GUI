1859433286fb3a36a7a9fd5ef462aaf70d1bf4fb
jwheller <jaheller@chapman.edu>
2021-10-22 13:53:50 -0700

    Initial Commit

---
+++ Maya_MultiCamManager_GUI.py
@@ -0,0 +1,57 @@
+#Maya Multi-Camera Manager plugin
+#by Jaden Heller
+
+import maya.cmds as cmds
+ 
+class MultiCamManager_Window(object):
+    
+    def __init__(self):
+        
+        self.window = "MultiCamManager"
+        self.title = "Multi-Camera Manager"
+        self.size = ( 400, 400)
+        loadCamBtnLabel = "Load All Cameras"
+        #Incase there's an instance of the window already open, close it
+        if cmds.window(self.window, exists = True):
+            cmds.deleteUI(self.window, window = True)
+        #Create a new instance of the window
+        self.window = cmds.window(self.window, title = self.title, widthHeight=self.size)
+        #Layout for the title and the seperator line
+        cmds.columnLayout(adjustableColumn = True)
+        cmds.text (self.title)
+        cmds.separator(height=20, style = "doubleDash")
+        #New layout for the Load Camera button (additional spacing between text and the button)
+        cmds.columnLayout(adjustableColumn = True, rowSpacing = 20) 
+        cmds.text("Load all cameras in the scene - Double-click to refresh")
+        cmds.button(label = "Load All Cameras", command = self.selectCameras)
+        #A collapsable menu called "Cameras" incase the user wishes to hide the list of cameras
+        self.CamFrame = cmds.frameLayout("camFrame", label = "Cameras", collapsable = True, collapse = False, marginWidth = 20)
+        cmds.columnLayout(adjustableColumn = True, columnOffset = ("both", 90), rowSpacing = 20) 
+
+        cmds.showWindow()
+    
+    
+    def selectCameras(self, *args):
+
+        shapeString = "Shape"
+        #List all the cameras in the scene
+        allCameras = cmds.ls(cameras = True) 
+        if len(allCameras) < 1:
+            cmds.warning("No Cameras in scene")
+        else:
+            self.clearButtons()
+            for cameraName in allCameras:
+                print (cameraName + " is a camera in the scene")
+                #Remove the word "-Shape" that Maya appends to the name of Camera objects:
+                cameraNameLabel = cameraName.replace(shapeString, "")  
+                lookThruCam = ("cmds.lookThru(\" %s \")") % (cameraName)
+                #Create the buttons
+                cmds.button(cameraName, label = cameraNameLabel, command = lookThruCam)
+
+
+    def clearButtons(self, *args):
+        
+        #Clear the frame holding the buttons so that it can be re-filled with updated Cam info
+        cmds.deleteUI(self.CamFrame, layout = True)
+        self.CamFrame = cmds.frameLayout("camFrame", label = "Cameras", collapsable = True, collapse = False, marginWidth = 20)
+        cmds.columnLayout(adjustableColumn = True, columnOffset = ("both", 90), rowSpacing = 20)
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # maya-camera-manager-GUI
 Multi-Camera Manager plug-in for Autodesk Maya
+
+A GUI that helps the user keep track of and easily switch between cameras in a scene. Allows the user to switch between any perspective with a click of one button as to speed up their workflow.
